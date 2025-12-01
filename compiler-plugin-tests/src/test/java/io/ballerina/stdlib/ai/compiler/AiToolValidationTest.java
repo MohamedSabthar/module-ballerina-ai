db874d32fe2a45ad88da06ae3091c29f7431a069
MohamedSabthar <SabtharUGC0@gmail.com>
2025-12-01 15:38:40 +0530

    Remove compiler plugin desugar

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/ai/compiler/AiToolValidationTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/ai/compiler/AiToolValidationTest.java
@@ -35,7 +35,6 @@ import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.Iterator;
 
-import static io.ballerina.stdlib.ai.plugin.diagnostics.CompilationDiagnostic.AGENT_MUST_BE_FINAL;
 import static io.ballerina.stdlib.ai.plugin.diagnostics.CompilationDiagnostic.CONTEXT_PARAM_MUST_BE_FIRST;
 import static io.ballerina.stdlib.ai.plugin.diagnostics.CompilationDiagnostic.INVALID_RETURN_TYPE_IN_TOOL;
 import static io.ballerina.stdlib.ai.plugin.diagnostics.CompilationDiagnostic.PARAMETER_IS_NOT_A_SUBTYPE_OF_ANYDATA;
@@ -164,17 +163,17 @@ public class AiToolValidationTest {
         assertErrorMessage(diagnostic, message, 67, 48);
     }
 
-    @Test
-    public void testModuleLevelAgentWithoutFinalQualifier() {
-        String packagePath = "06_module_level_agent_without_final_qualifier";
-        DiagnosticResult diagnosticResult = getDiagnosticResult(packagePath);
-        Assert.assertEquals(diagnosticResult.errorCount(), 1);
+    // @Test
+    // public void testModuleLevelAgentWithoutFinalQualifier() {
+    //     String packagePath = "06_module_level_agent_without_final_qualifier";
+    //     DiagnosticResult diagnosticResult = getDiagnosticResult(packagePath);
+    //     Assert.assertEquals(diagnosticResult.errorCount(), 1);
 
-        Iterator<Diagnostic> diagnosticIterator = diagnosticResult.errors().iterator();
-        Diagnostic diagnostic = diagnosticIterator.next();
-        String message = getErrorMessage(AGENT_MUST_BE_FINAL);
-        assertErrorMessage(diagnostic, message, 105, 1);
-    }
+    //     Iterator<Diagnostic> diagnosticIterator = diagnosticResult.errors().iterator();
+    //     Diagnostic diagnostic = diagnosticIterator.next();
+    //     String message = getErrorMessage(AGENT_MUST_BE_FINAL);
+    //     assertErrorMessage(diagnostic, message, 105, 1);
+    // }
 
     private DiagnosticResult getDiagnosticResult(String path) {
         Path projectDirPath = RESOURCE_DIRECTORY.resolve(path);
